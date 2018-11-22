af840d643cff1089ed683250414b05dbf13fcdc8
Henri Koski <henri.t.koski@gmail.com>
2018-11-22 18:15:21 +0200

    Make api test parallel

--- api_test.go
+++ api_test.go
@@ -17,33 +17,38 @@ import (
 func TestListAPI(t *testing.T) {
 	handler := todo.NewHandler(todo.NewList())
 
-	taskData := todo.TaskData{
-		Name:      "my task",
-		CreatedAt: time.Now().Round(0), // strip monotonic clock
+	for i := 0; i < 2; i++ {
+		t.Run(fmt.Sprint(i), func(t *testing.T) {
+			t.Parallel()
+			taskData := todo.TaskData{
+				Name:      "my task",
+				CreatedAt: time.Now().Round(0), // strip monotonic clock
+			}
+			body, err := json.Marshal(taskData)
+			require.NoError(t, err, "Failed to marshal task data")
+
+			req := httptest.NewRequest("POST", "http://localhost/api/tasks", bytes.NewBuffer(body))
+			w := httptest.NewRecorder()
+
+			handler.ServeHTTP(w, req)
+			assert.Equal(t, http.StatusOK, w.Result().StatusCode, "Unexpected status code after posting new task")
+
+			var id uint64
+			err = json.NewDecoder(w.Result().Body).Decode(&id)
+			require.NoError(t, err, "Failed to decode response body to uint64")
+
+			req = httptest.NewRequest("GET", fmt.Sprintf("http://localhost/api/tasks/%d", id), nil)
+			w = httptest.NewRecorder()
+
+			handler.ServeHTTP(w, req)
+			assert.Equal(t, http.StatusOK, w.Result().StatusCode, "Unexpected status code after getting task")
+
+			task := todo.Task{}
+			err = json.NewDecoder(w.Result().Body).Decode(&task)
+			require.NoError(t, err, "Failed to decode response body to task")
+			assert.Equal(t, todo.Task{ID: id, TaskData: taskData}, task, "Task data doesn't match")
+		})
 	}
-	body, err := json.Marshal(taskData)
-	require.NoError(t, err, "Failed to marshal task data")
-
-	req := httptest.NewRequest("POST", "http://localhost/api/tasks", bytes.NewBuffer(body))
-	w := httptest.NewRecorder()
-
-	handler.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Result().StatusCode, "Unexpected status code after posting new task")
-
-	var id uint64
-	err = json.NewDecoder(w.Result().Body).Decode(&id)
-	require.NoError(t, err, "Failed to decode response body to uint64")
-
-	req = httptest.NewRequest("GET", fmt.Sprintf("http://localhost/api/tasks/%d", id), nil)
-	w = httptest.NewRecorder()
-
-	handler.ServeHTTP(w, req)
-	assert.Equal(t, http.StatusOK, w.Result().StatusCode, "Unexpected status code after getting task")
-
-	task := todo.Task{}
-	err = json.NewDecoder(w.Result().Body).Decode(&task)
-	require.NoError(t, err, "Failed to decode response body to task")
-	assert.Equal(t, todo.Task{ID: id, TaskData: taskData}, task, "Task data doesn't match")
 }
 
 func TestAPIServer(t *testing.T) {
